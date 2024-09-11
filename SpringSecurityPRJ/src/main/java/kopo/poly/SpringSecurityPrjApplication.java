4c2ad5ca90c11297cae26ca4a10edf408cf84820
junsu97 <wnstn7759@naver.com>
2024-09-11 11:40:01 +0900

    Eureka

--- SpringSecurityPRJ/src/main/java/kopo/poly/SpringSecurityPrjApplication.java
+++ SpringSecurityPRJ/src/main/java/kopo/poly/SpringSecurityPrjApplication.java
@@ -2,10 +2,13 @@ package kopo.poly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+
 public class SpringSecurityPrjApplication {
 
     public static void main(String[] args) {
