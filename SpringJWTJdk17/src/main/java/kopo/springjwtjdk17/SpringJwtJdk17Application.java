b66cb4068ac5e644db8a55b66a8aa40757748181
junsu97 <wnstn7759@naver.com>
2024-09-24 13:06:04 +0900

    Create SpringJWTPRJ

---
+++ SpringJWTJdk17/src/main/java/kopo/springjwtjdk17/SpringJwtJdk17Application.java
@@ -0,0 +1,13 @@
+package kopo.springjwtjdk17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJwtJdk17Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJwtJdk17Application.class, args);
+    }
+
+}
---
+++ SpringJWTJdk17/src/test/java/kopo/springjwtjdk17/SpringJwtJdk17ApplicationTests.java
@@ -0,0 +1,13 @@
+package kopo.springjwtjdk17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJwtJdk17ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
